cdb654a2c74b2dbc35e2d0a78105478908eccdd1
boyindia <boyindia11@gmail.com>
2024-01-12 16:33:04 +0800

    feat: adjust deposite card

--- examples/multi-connect/src/GGC.tsx
+++ examples/multi-connect/src/GGC.tsx
@@ -1,5 +1,5 @@
 import { Button, Card } from 'antd';
-import { formatGwei, parseGwei } from 'viem';
+import { formatEther, parseEther } from 'viem';
 import { useAccount, useContractRead, useContractWrite } from 'wagmi';
 
 import ERC20ABI from './abi/erc20.json';
@@ -7,38 +7,35 @@ import NODEABI from './abi/node.json';
 import { contractAddress } from './const';
 import styles from './index.module.css';
 
+const gameAddress = '0xa34357486224151ddfdb291e13194995c22df505';
 function GGC() {
   const { address, isConnected } = useAccount();
   const { data: allowance, isLoading } = useContractRead({
     address: contractAddress.ERC20 as `0x${string}`,
     abi: ERC20ABI,
     functionName: 'allowance',
-    args: [address, contractAddress.ERC20 as `0x${string}`],
+    args: [address, contractAddress.NODE as `0x${string}`],
   });
 
-  const { write: resourceIn } = useContractWrite({
+  const { write: resourceIn, status: resouceInStatus } = useContractWrite({
     address: contractAddress.NODE as `0x${string}`,
     abi: NODEABI,
     functionName: 'resourceIn',
   });
 
-  const {
-    status: approveStatus,
-    isLoading: approveLoading,
-    write: makeApprove,
-  } = useContractWrite({
+  const { status: approveStatus, write: makeApprove } = useContractWrite({
     address: contractAddress.ERC20 as `0x${string}`,
     abi: ERC20ABI,
     functionName: 'approve',
   });
 
   const handleDeposite = async () => {
-    await makeApprove({ args: [address, parseGwei('100')] });
+    await makeApprove({ args: [contractAddress.NODE, parseEther('100')] });
   };
 
   const handleResourceIn = async () => {
     await resourceIn({
-      args: ['0xa34357486224151ddfdb291e13194995c22df505', parseGwei('100')],
+      args: [gameAddress, parseEther('100')],
     });
   };
 
@@ -49,13 +46,14 @@ function GGC() {
   return (
     <Card className={styles.space}>
       <div className={styles.space}>
-        allowance: {isLoading ? 'loading...' : formatGwei(allowance as bigint)}
+        allowance: {isLoading ? 'loading...' : formatEther(allowance as bigint)} GGC
       </div>
       <Button style={{ marginRight: '12px' }} onClick={handleDeposite}>
-        Apprve 100GGC
+        Approve 100GGC
       </Button>
       <Button onClick={handleResourceIn}>deposit 100GGC</Button>
       <div className={styles.space}>approveStatus: {approveStatus}</div>
+      <div className={styles.space}>resouceInStatus: {resouceInStatus}</div>
     </Card>
   );
 }
