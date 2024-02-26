c3ba2f63a0e332fe08fd901819ca862256374912
andy00614 <33732843@qq.com>
2024-02-26 09:38:51 +0800

    fix: connectHOC update

--- packages/core/src/components/connect-hoc/index.tsx
+++ packages/core/src/components/connect-hoc/index.tsx
@@ -1,16 +1,27 @@
 import type { ComponentType } from 'react';
 import React from 'react';
+import { useWeb3Modal } from '@web3modal/wagmi/react';
+import { Button } from 'antd';
 
-import { MixConnectButton, useUnifiedAccount } from '../..';
+import { useUnifiedAccount } from '../..';
 
 export function ConnectButtonHOC<
   P extends JSX.IntrinsicAttributes & { children?: React.ReactNode },
 >(Component: ComponentType<P>): ComponentType<P> {
   return function WrappedComponent(props: P) {
     const account = useUnifiedAccount();
+    const { open } = useWeb3Modal();
 
     if (!account.isConnected) {
-      return <MixConnectButton />;
+      return (
+        <Button
+          onClick={() => {
+            open();
+          }}
+        >
+          Wallet Connect
+        </Button>
+      );
     }
 
     return <Component {...props} />;
