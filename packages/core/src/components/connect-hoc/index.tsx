d66bd63d30b0dea49b5e69f16355d17918d16ea8
andy00614 <33732843@qq.com>
2024-04-02 14:03:42 +0800

    feat: update connecthoc function

--- packages/core/src/components/connect-hoc/index.tsx
+++ packages/core/src/components/connect-hoc/index.tsx
@@ -1,3 +1,32 @@
+/**
+ * ConnectButtonHOC is a higher-order component that wraps a component and provides wallet connection functionality.
+ * It renders the wrapped component when the wallet is connected, and renders a connect button or custom disconnected content when the wallet is disconnected.
+ *
+ * @param Component - The component to be wrapped, which will be rendered when the wallet is connected.
+ * @param title - The title or text to be displayed on the connect button when the wallet is disconnected.
+ * @returns A new component that includes the wallet connection functionality.
+ *
+ * @example
+ * const CustomConnectButton = ConnectButtonHOC(Button, 'Connect Wallet');
+ *
+ * <CustomConnectButton>
+ *   Connected content
+ * </CustomConnectButton>
+ *
+ * @example
+ * const CustomConnectButton = ConnectButtonHOC(Button, 'Connect Wallet');
+ *
+ * <CustomConnectButton
+ *   renderDisconnected={(open) => (
+ *     <div onClick={open}>
+ *       Custom disconnected content
+ *     </div>
+ *   )}
+ * >
+ *   Connected content
+ * </CustomConnectButton>
+ */
+
 import type { ComponentType } from 'react';
 import React from 'react';
 import { useWeb3Modal } from '@levellink/wagmimodal/react';
@@ -5,14 +34,22 @@ import { Button } from 'antd';
 
 import { useUnifiedAccount } from '../..';
 
+type ConnectButtonHOCProps<P> = P & {
+  renderDisconnected?: (open: () => void) => React.ReactNode;
+};
+
 export function ConnectButtonHOC<
   P extends JSX.IntrinsicAttributes & { children?: React.ReactNode },
->(Component: ComponentType<P>, title: string): ComponentType<P> {
-  return function WrappedComponent(props: P) {
+>(Component: ComponentType<P>, title?: string): ComponentType<ConnectButtonHOCProps<P>> {
+  return function WrappedComponent(props: ConnectButtonHOCProps<P>) {
     const account = useUnifiedAccount();
     const { open } = useWeb3Modal();
 
     if (!account.isConnected) {
+      if (props.renderDisconnected) {
+        return <>{props.renderDisconnected(open)}</>;
+      }
+
       return (
         <Button
           onClick={() => {
