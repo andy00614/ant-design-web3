8eeb8a63a530f15b024add58310cb7c59acd18ee
andy00614 <33732843@qq.com>
2024-04-02 14:35:25 +0800

    fix: update type

--- packages/core/src/components/connect-hoc/index.tsx
+++ packages/core/src/components/connect-hoc/index.tsx
@@ -1,32 +1,3 @@
-/**
- * ConnectButtonHOC is a higher-order component that wraps a component and provides wallet connection functionality.
- * It renders the wrapped component when the wallet is connected, and renders a connect button or custom disconnected content when the wallet is disconnected.
- *
- * @param Component - The component to be wrapped, which will be rendered when the wallet is connected.
- * @param title - The title or text to be displayed on the connect button when the wallet is disconnected.
- * @returns A new component that includes the wallet connection functionality.
- *
- * @example
- * const CustomConnectButton = ConnectButtonHOC(Button, 'Connect Wallet');
- *
- * <CustomConnectButton>
- *   Connected content
- * </CustomConnectButton>
- *
- * @example
- * const CustomConnectButton = ConnectButtonHOC(Button, 'Connect Wallet');
- *
- * <CustomConnectButton
- *   renderDisconnected={(open) => (
- *     <div onClick={open}>
- *       Custom disconnected content
- *     </div>
- *   )}
- * >
- *   Connected content
- * </CustomConnectButton>
- */
-
 import type { ComponentType } from 'react';
 import React from 'react';
 import { useWeb3Modal } from '@levellink/wagmimodal/react';
@@ -38,9 +9,10 @@ type ConnectButtonHOCProps<P> = P & {
   renderDisconnected?: (open: () => void) => React.ReactNode;
 };
 
-export function ConnectButtonHOC<
-  P extends JSX.IntrinsicAttributes & { children?: React.ReactNode },
->(Component: ComponentType<P>, title?: string): ComponentType<ConnectButtonHOCProps<P>> {
+export function ConnectButtonHOC<P>(
+  Component: ComponentType<P>,
+  title?: string,
+): ComponentType<ConnectButtonHOCProps<P>> {
   return function WrappedComponent(props: ConnectButtonHOCProps<P>) {
     const account = useUnifiedAccount();
     const { open } = useWeb3Modal();
